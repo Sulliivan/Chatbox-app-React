95879dabd16f194852ef02fb5fa1ae8932feea65
Sulliivan <64897675+Sulliivan@users.noreply.github.com>
2020-11-15 19:43:33 +0100

    config firebase

---
+++ src/base.js
@@ -0,0 +1,15 @@
+import Rebase from 're-base'
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+const firebaseApp = firebase.initializeApp({ 
+    apiKey: "AIzaSyCBxFTki7Q-rs-oFN4fQh1CXWZ4Ci_obvw",
+    authDomain: "chatbox-5753f.firebaseapp.com",
+    databaseURL: "https://chatbox-5753f.firebaseio.com",
+})
+
+const base = Rebase.createClass(firebase.database())
+
+export { firebaseApp }
+
+export default base
