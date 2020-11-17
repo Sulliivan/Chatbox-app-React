6f7c898fe91f5d674b9aa492cf438ed5ef2828f7
Sulliivan <64897675+Sulliivan@users.noreply.github.com>
2020-11-17 11:24:32 +0100

    Chatbox firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import './App.css'
 
 import Formulaire from './components/Formulaire'
@@ -13,26 +13,43 @@ class App extends Component {
     pseudo: this.props.match.params.pseudo
   }
 
+  messagesRef = createRef()
+
   componentDidMount(){
     base.syncState('/', {
       context: this,
       state: 'messages'
     })
   }
-//
+
+  componentDidUpdate(){
+    const ref = this.messagesRef.current
+    ref.scrollTop = ref.scrollHeight // la barre de scroll reste en bas
+  }
 
   addMessage = message => {
-    const messages = { ...this.state.messages}
-    messages[`message-${Date.now()}`] = message
+    const messages = { ...this.state.messages} // copy du state
+
+    messages[`message-${Date.now()}`] = message // applique les changements
+    Object
+      .keys(messages)
+      .slice(0, -10)
+      .forEach(key => {
+        messages[key] = null
+      })
+
     this.setState({ messages })
   }
+
+  isUser = pseudo => pseudo === this.state.pseudo
 
   render () {
     const messages = Object
     .keys(this.state.messages)
-    .map(key => (
+    .map(key => (  // .map modifie ou retourne quelque chose
       <Message
         key={key}
+        isUser={this.isUser}
         message={this.state.messages[key].message}
         pseudo={this.state.messages[key].pseudo}
       />
@@ -40,7 +57,7 @@ class App extends Component {
     return (
       <div className='box'>
         <div>
-          <div className="messages">
+          <div className="messages" ref={this.messagesRef}>
             <div className="message">
               { messages }
             </div>
